ac186c450b42dcd090811ef9a887804dc948a2fb
dimman <fog@udden.net>
2013-03-14 22:39:57 +0100

    STAT_MATCHSTARTTIME gameclock sync implemented
    32bit build fix. Remove leftover assembly reference.

--- common.h
+++ common.h
@@ -71,6 +71,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define	STAT_ITEMS			15
 #define STAT_VIEWHEIGHT		16		// Z_EXT_VIEWHEIGHT protocol extension
 #define STAT_TIME			17		// Z_EXT_TIME extension
+#define STAT_MATCHSTARTTIME     18
 
 // item flags
 #define	IT_SHOTGUN			1
